b373ca38f5df8e6e621005157ddb4b5031330e19
wswzgdg <wswzgdg@gmail.com>
2026-02-02 13:10:14 +0800

    同步模块更新并拆分卸载脚本

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,9 +1,2 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
-resetprop -n persist.oplus.display.vrr 0
-resetprop -n persist.oplus.display.vrr.adfr 0
-resetprop -n debug.oplus.display.dynamic_fps_switch 0
-resetprop -n sys.display.vrr.vote.support 0
-resetprop -n vendor.display.enable_dpps_dynamic_fps 0
-resetprop -n ro.display.brightness.brightness.mode 1
-setprop debug.egl.swapinterval 1
